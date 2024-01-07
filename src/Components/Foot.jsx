c1408cbea789b10dec0be8dfae1417e65ca4c849
Judith Kraziewicz <judith.kraziewicz@gmail.com>
2024-01-07 20:45:10 +0000

    change to files

--- src/Components/Foot.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Foot(){
-    return (
-        <footer className='foot'>
-            <p>&copy; Judith Kraziewicz</p>
-        </footer>
-    );
-}
